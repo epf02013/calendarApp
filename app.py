71f1730126fff2b365c246bb07ff3026d5df4e72
Tim Kaye <timothy.kaye@pomona.edu>
2015-04-19 21:14:00 -0700

    cal page sketchs

--- app.py
+++ app.py
@@ -10,19 +10,22 @@ app = Flask(__name__)
 @app.route("/")
 def index() :
 	user_id = session.get("user_id")
-	if not user_id :
-		return render_template("index.html")
-	else :
-		user = db.User.get_from_id(user_id)
+	
+	#if not user_id :
+	#	return render_template("login.html")
+	#else:
+	#	user = db.User.get_from_id(user_id)
 
-		cal  = calendar.Calendar()
-		
-		year = time.localtime()[0]
-		month = time.localtime()[1]
+	cal  = calendar.Calendar()
+	
+	year = time.localtime()[0]
+	month = time.localtime()[1]
 
-		days = [cal.monthdatescalendar(year, month + 1)]
+	days = cal.monthdatescalendar(year, month + 1)
 
-		return render_template("calendar.html", days = days)
+	print days
+
+	return render_template("calendar.html", days = days)
 
 
 
