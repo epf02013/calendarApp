3ac12df6ef4b17f0ef1ada5d408977a0c6608be8
Tim Kaye <timothy.kaye@pomona.edu>
2015-04-19 19:22:37 -0700

    nothing really

---
+++ app.py
@@ -0,0 +1,30 @@
+import datetime
+import calendar
+import time
+from flask import Flask, render_template, request, session, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index() :
+	user_id = session.get("user_id")
+	if not user_id :
+		return render_template("index.html")
+	else :
+		user = db.User.get_from_id(user_id)
+
+		cal  = calendar.Calendar()
+		
+		year = time.localtime()[0]
+		month = time.localtime()[1]
+
+		days = [cal.monthdatescalendar(year, month + 1)]
+
+		return render_template("calendar.html", days = days)
+
+
+
+if __name__ == "__main__" : 
+	app.run(debug=True)
