a17b3bb23c45d60adf2b1512d108cb5a968f554d
epf02013 <ecpraterfahey@gmail.com>
2015-04-23 17:32:24 -0700

    updated login database

--- app.py
+++ app.py
@@ -28,50 +28,76 @@ def connect_db():
     	return Bucket(dbName)
 db=connect_db()
 
-@app.route("/signup")
-def signup():
-	return render_template("signup.html")
+#@app.route("/signup")
+#def signup():
+	#return render_template("signup.html")
 
-@app.route("/registerUser", methods=['GET', 'POST'])
-def registerUser():
-        doc={}
-	for k, v in form.items():
-		doc[k]=v
-	if not 'firstName' in doc or not doc['firstName']:
-		return None, ("Must have First Name", 400)
-	if not 'lastName' in doc or not doc['lastName']:
-		return None, ("Must have Last Name", 400)
-	if not 'email' in doc or not doc['email']:
-		return None, ("Must have email", 400)
-	if not 'age' in doc or not doc['age']:
-		return None, ("Must have age", 400)
+#@app.route("/registerUser")
+#def registerUser(form):
+#	doc={}
+#	for k, v in form.items():
+#		doc[k]=v
+#	if not 'firstName' in doc or not doc['firstName']:
+#		return None, ("Must have First Name", 400)
+#	if not 'lastName' in doc or not doc['lastName']:
+#		return None, ("Must have Last Name", 400)
+#	if not 'email' in doc or not doc['email']:
+#		return None, ("Must have email", 400)
+#	if not 'age' in doc or not doc['age']:
+#		return None, ("Must have age", 400)
 	
-	try:
-		db.add(doc['email'],doc)
-		return redirect("/registered")
-	except KeyExistsError:
-		return "Sorry that email is already in use", 400
-        return render_template("registerUser.html")
+#	try:
+#		db.add(doc['email'],doc)
+#		return redirect("/registered")
+#	except KeyExistsError:
+#		return "Sorry that email is already in use", 400
 
+@app.route("/logged_in", methods=['GET', 'POST'])
+def logged_in():
+        session['logged_in']=True
+        temp=""+request.form['user_info']
+        user_info=json.loads(temp)
+        user_info['access_token']=request.form['access_token']
+        user_id=user_info['id']
+        try :
+                db.add(user_id, json.dumps(user_info))
+        except KeyExistsError:
+                doc=db.get(user_id)
+                doc=doc.value
+                print(temp)
+                doc=json.loads(doc)
+                del doc['access_token']
+                doc['access_token']=user_info['access_token']
+                db.set(user_id, json.dumps(doc))
 
+        return render_template("logged_in.html", logged_in="trueuu")
 @app.route("/")
 def index() :
 	user_id = session.get("user_id")
-	if not user_id :
-		return render_template("index.html")
-	else :
-		user = db.User.get_from_id(user_id)
+	
+	#if not user_id :
+	#	return render_template("login.html")
+	#else:
+	#	user = db.User.get_from_id(user_id)
 
-		cal  = calendar.Calendar()
-		
-		year = time.localtime()[0]
-		month = time.localtime()[1]
+	cal  = calendar.Calendar()
+	
+	year = time.localtime()[0]
+	month = time.localtime()[1]
 
-		days = [cal.monthdatescalendar(year, month + 1)]
+	days = cal.monthdatescalendar(year, month + 1)
 
-		return render_template("calendar.html", days = days)
+	print days
+        try:
+                if (session['logged_in']) :
+                        return render_template("calendar.html", days = days)
+                else:
+                        return render_template("login.html")
+        except KeyError:
+                        return render_template("login.html")
 
 if __name__ == "__main__" : 
+        app.secret_key="secretive_key"
 	app.run(debug=True)
-	
+	sess.init_app(app)
 	
