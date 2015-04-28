b2c6a738b8dc0a10af297a281937bc103072a0d4
epf02013 <ecpraterfahey@gmail.com>
2015-04-27 19:58:34 -0700

    stuff

--- fb_grabber.py
+++ fb_grabber.py
@@ -2,41 +2,59 @@ import json
 import requests
 import arrow
 import datetime
-from config import FB_ACCESS_TOKEN
 from classifier import get_sentiment
 
+
+
+
+
+#takes stored events and returns old events + new events as json
+def updateEvents(user_id, FB_AT, prevEvents) :
+    eventsFromFB=json.loads(get_fb_events(user_id, FB_AT))
+    prevEvents=json.loads(prevEvents)
+    for fbEvent in eventsFromFB :
+        isNew=True
+        for prevEvent in prevEvents :
+            if fbEvent['event_id']==prevEvent['event_id'] :
+                isNew=False
+        if isNew :
+            prevEvents.append(fbEvent)
+    return prevEvents
+
+        
+        
 # facebook data extract 
-def get_fb_events(user_id) :
-	# store all events in an array
-	events = []
-
-	# the base URL to query
-	url = "https://graph.facebook.com/v2.3/%s/events?access_token=%s&pretty=1" % (user_id, FB_ACCESS_TOKEN)
-
-	# make a GET 
-	response = requests.get(url)
-	if response.status_code == requests.codes.ok :
-		# decode the JSON
-		resp_body = response.json()
-		if "data" in resp_body.keys() : 
-			for event in resp_body["data"] :
-				# we only care about events that you plan on attending
-				if event["rsvp_status"] == "attending" :
-					events.append(format_event(event))
-
-		# check for events on the second page 
+def get_fb_events(user_id, FB_ACCESS_TOKEN) :
+    # store all events in an array
+    events = []
+    
+    # the base URL to query
+    url = "https://graph.facebook.com/v2.3/%s/events?access_token=%s&pretty=1" % (user_id, FB_ACCESS_TOKEN)
+    
+    # make a GET 
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok :
+        # decode the JSON
+        resp_body = response.json()
+        if "data" in resp_body.keys() : 
+            for event in resp_body["data"] :
+                # we only care about events that you plan on attending
+                if event["rsvp_status"] == "attending" :
+                    events.append(format_event(event))
+                    
+                # check for events on the second page 
 		if "paging" in resp_body.keys() :
-			if "next" in resp_body["paging"].keys() :
-				next_page = requests.get(resp_body["paging"]["next"])
-				if next_page.status_code == requests.codes.ok :
-					month = datetime.datetime.now().month
-					for event in next_page.json()["data"] :
-						if arrow.get(event["start_time"]).month == month :
-							events.append(format_event(event))
-	else :
+                    if "next" in resp_body["paging"].keys() :
+                        next_page = requests.get(resp_body["paging"]["next"])
+                        if next_page.status_code == requests.codes.ok :
+                            month = datetime.datetime.now().month
+                            for event in next_page.json()["data"] :
+                                if arrow.get(event["start_time"]).month == month :
+                                    events.append(format_event(event))
+    else :
 		print "access code expired :("
 
-	return json.dumps(events)
+    return json.dumps(events)
 
 
 def format_event(event) :
@@ -53,8 +71,3 @@ def format_event(event) :
 	new_event["sentiment"] = response["sentiment"]
 	
 	return new_event
-
-
-
-if __name__ == "__main__" :
-	print get_fb_events("10152889999328269")
